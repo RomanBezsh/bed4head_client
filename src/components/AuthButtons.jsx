4d74c03dff8f6b8871b87d8a01dcbd750b6d00d4
Romchik <Romchik@DESKTOP-9NTFBF8>
2026-02-24 12:27:35 +0200

    Done Header.jsx and AuthButtons.jsx

---
+++ src/components/AuthButtons.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const AuthButtons = ({ accountIcon, isLoggedIn = false }) => {
+    const baseBtn = "h-[32px] border border-border-gray rounded-full bg-[#FFFFFF] font-nunito-sans text-[16px] font-normal flex items-center";
+
+    return (
+        <div className="flex items-center gap-[10px]">
+            {!isLoggedIn ? (
+                <>
+                    {/* Register */}
+                    <button className={`${baseBtn} w-[92px] justify-center`}>
+                        Register
+                    </button>
+
+                    {/* Sign In */}
+                    <button className={`${baseBtn} w-[120px] pl-4 pr-1 justify-between`}>
+                        <span className="ml-[10px]">Sign In</span>
+                        <div className="w-6 h-6 bg-[#666666] rounded-full flex items-center justify-center">
+                            <img src={accountIcon} alt="account" className="w-4 h-4" />
+                        </div>
+                    </button>
+                </>
+            ) : (
+                /* Account if user is logged in */
+                <button className={`${baseBtn} w-[120px] pl-4 pr-1 justify-between`}>
+                    <span className="ml-[10px]">Account</span>
+                    <div className="w-6 h-6 bg-[#666666] rounded-full flex items-center justify-center">
+                        <img src={accountIcon} alt="account" className="w-4 h-4" />
+                    </div>
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default AuthButtons;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
 import logo from "/logo.svg";
+import accountIcon from "/icons/account_icon.svg";
+import AuthButtons from "./AuthButtons.jsx";
 
-export const Header = () => {
+const Header = () => {
     return (
         <header className="w-full h-[80px] flex items-center justify-center bg-white">
             <div className="w-full max-w-[1440px] px-8 flex items-center justify-between">
 
+                {/*Logo*/}
                 <img
                     src={logo}
                     alt="Logo"
                     className="w-[167px] h-[33px]"
                 />
 
-                <div className="flex items-center gap-4">
 
+                <div className="flex items-center gap-[10px]">
+                    {/*Change language*/}
                     <button
-                        className="w-[32px] h-[32px] border-none overflow-hidden
+                        className="w-[26px] h-[26px] border-none rounded-full overflow-hidden
                        hover:opacity-80 transition-opacity
                        flex items-center justify-center"
                     >
@@ -25,16 +29,14 @@ export const Header = () => {
                         />
                     </button>
 
-                    <button className="px-4 py-1 border rounded-full text-sm hover:bg-gray-100 transition">
-                        Register
-                    </button>
-
-                    <button className="px-4 py-1 border rounded-full text-sm hover:bg-gray-100 transition">
-                        Sign In
-                    </button>
+                    {/*Auth buttons*/}
+                    <AuthButtons accountIcon={accountIcon} />
 
                 </div>
             </div>
         </header>
     );
 };
+
+
+export default Header;
