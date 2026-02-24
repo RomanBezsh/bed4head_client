b6269397b8b4aea68c3cf538fba5a6ac5e24eda4
Romchik <Romchik@DESKTOP-9NTFBF8>
2026-02-24 12:26:31 +0200

    Changed Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import {Header} from './components/Header'
+import Header from './components/Header'
 function App() {
 
 
@@ -8,13 +8,9 @@ function App() {
     return(
         <div className="App h-auto">
             <Header></Header>
+
         </div>
     );
-  // return (
-  //     <div className="min-h-screen bg-blue-400 flex items-center justify-center text-white">
-  //       <h1 className="text-4xl font-bold">Привет, Tailwind!</h1>
-  //     </div>
-  // );
 }
 
 export default App
