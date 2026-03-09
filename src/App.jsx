990379622f9e159a553d92552960421181084aa6
Romchik <Romchik@DESKTOP-9NTFBF8>
2026-03-09 21:13:24 +0200

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-// import { useState } from 'react'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+// import Catalog from './pages/Catalog' 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Catalog from "./components/Catalog.jsx";
+
 function App() {
+    return (
+        <BrowserRouter>
+            <div className="App h-auto font-nunito-sans">
+                <Header />
 
-
-
-
-    return(
-        <div className="App h-auto font-nunito-sans">
-            <Header></Header>
-            <MainContent></MainContent>
-            
-        </div>
+                <Routes>
+                    <Route path="/" element={<MainContent />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
-export default App
+export default App;
