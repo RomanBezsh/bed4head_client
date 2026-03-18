9362974ce9bd2883809d3c370d9ec2ca31f8c0cc
RomanPro <RomanPro@PC>
2026-03-18 20:05:46 +0200

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/common/Header.jsx";
 import Home from "./pages/Home.jsx";
 import Catalog from "./pages/Catalog.jsx";
 import AccountDashboard from "./pages/AccountDashboard.jsx";
+import HotelPage from "./pages/HotelPage.jsx";
+import Booking from "./pages/Booking.jsx";
+
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App h-auto font-nunito-sans">
-                <Header />
-
+        <div className="App h-auto font-nunito-sans">
+            <BrowserRouter>
+                <Header/>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/catalog" element={<Catalog />} />
-                    <Route path="/account" element={<AccountDashboard />} />
+                    <Route
+                        path="/"
+                        element={
+                            <Home/>
+                        }
+                    />
+                    <Route
+                        path="/catalog"
+                        element={
+                            <Catalog/>
+                        }
+                    />
+                    <Route
+                        path="/hotel"
+                        element={
+                            <HotelPage/>
+                        }
+                    />
+                    <Route
+                        path="/account"
+                        element={<AccountDashboard/>}
+                    />
+                    <Route
+                        path="/booking"
+                        element={<Booking/>}
+                    />
                 </Routes>
-            </div>
-        </BrowserRouter>
+            </BrowserRouter>
+        </div>
     );
 }
 
--- src/components/common/Auth/AuthButtons.jsx
+++ src/components/common/Auth/AuthButtons.jsx
@@ -16,17 +16,10 @@ const AuthButtons = ({ accountIcon, isLoggedIn = false, onOpen }) => {
                         Register
                     </button>
 
-                    <button
-                        onClick={() => onOpen && onOpen("login")}
-                        className={`${baseBtn} w-[120px] pl-4 pr-1 justify-between`}
-                    >
-                        <span className="ml-[10px]">Sign In</span>
-
-                        <div
-                            className="w-6 h-6 bg-[#666666] rounded-full flex items-center justify-center cursor-pointer"
-                            onClick={() => navigate("/account")}
-                        >
-                            <img src={accountIcon} alt="account" className="w-4 h-4" />
+                    <button onClick={() => onOpen("login")} className={`${baseBtn} w-[120px] pr-1 justify-between`}>
+                        <span className="ml-[16px]">Sign In</span>
+                        <div className="w-6 h-6 my-1 mr-[5px] bg-[#666666] rounded-full flex items-center justify-center">
+                            <img src={accountIcon} alt="account" className="w-6 h-6" />
                         </div>
                     </button>
                 </>
@@ -34,10 +27,7 @@ const AuthButtons = ({ accountIcon, isLoggedIn = false, onOpen }) => {
                 <button className={`${baseBtn} w-[120px] pl-4 pr-1 justify-between`}>
                     <span className="ml-[10px]">Account</span>
 
-                    <div
-                        className="w-6 h-6 bg-[#666666] rounded-full flex items-center justify-center cursor-pointer"
-                        onClick={() => navigate("/account")}
-                    >
+                    <div className="w-6 h-6 bg-[#666666] rounded-full flex items-center" onClick={() => navigate("/account")}>
                         <img src={accountIcon} alt="account" className="w-4 h-4" />
                     </div>
                 </button>
