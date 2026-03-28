618257a83fb4431d15942f07b2b7f5ade8925257
RomanPro <RomanPro@PC>
2026-03-28 11:08:24 +0200

    Fixed

--- src/components/common/Auth/AuthButtons.jsx
+++ src/components/common/Auth/AuthButtons.jsx
@@ -6,26 +6,26 @@ const AuthButtons = ({ accountIcon, isLoggedIn = false, onOpen }) => {
     const baseBtn = "h-[32px] border border-gray rounded-full bg-[#FFFFFF] font-nunito-sans text-[16px] font-normal flex items-center";
 
     return (
-        <div className="flex items-center gap-[10px]">
+        <div className="flex items-center gap-2.5">
             {!isLoggedIn ? (
                 <>
                     <button
                         onClick={() => onOpen && onOpen("register")}
-                        className={`${baseBtn} w-[92px] justify-center`}
+                        className={`${baseBtn} w-23 justify-center`}
                     >
                         Register
                     </button>
 
-                    <button onClick={() => onOpen("login")} className={`${baseBtn} w-[120px] pr-1 justify-between`}>
-                        <span className="ml-[16px]">Sign In</span>
-                        <div className="w-6 h-6 my-1 mr-[5px] bg-[#666666] rounded-full flex items-center justify-center">
+                    <button onClick={() => onOpen("login")} className={`${baseBtn} w-30 pr-1 justify-between`}>
+                        <span className="ml-4">Sign In</span>
+                        <div className="w-6 h-6 my-1 mr-1.25 bg-[#666666] rounded-full flex items-center justify-center">
                             <img src={accountIcon} alt="account" className="w-6 h-6" />
                         </div>
                     </button>
                 </>
             ) : (
-                <button className={`${baseBtn} w-[120px] pl-4 pr-1 justify-between`}>
-                    <span className="ml-[10px]">Account</span>
+                <button className={`${baseBtn} w-30 pl-4 pr-1 justify-between`}>
+                    <span className="ml-2.5">Account</span>
 
                     <div className="w-6 h-6 bg-[#666666] rounded-full flex items-center" onClick={() => navigate("/account")}>
                         <img src={accountIcon} alt="account" className="w-4 h-4" />
--- src/components/hotelPage/ReviewStats.jsx
+++ src/components/hotelPage/ReviewStats.jsx
@@ -1,83 +1,72 @@
 const ReviewStats = () => {
+  const stats = [
+    { label: "Facilities", score: 9.8 },
+    { label: "Staff", score: 7.9 },
+    { label: "Cleanliness", score: 9.2 },
+    { label: "Comfort", score: 9.5 },
+    { label: "Location", score: 9.2 },
+    { label: "Value for money", score: 9.8 },
+  ];
 
-    const stats = [
-        {label: "Facilities", score: 9.8},
-        {label: "Staff", score: 7.9},
-        {label: "Cleanliness", score: 9.2},
-        {label: "Comfort", score: 9.5},
-        {label: "Location", score: 9.2},
-        {label: "Value for money", score: 9.8},
-    ];
-
-    return (
-        <div className="w-full max-w-275 mt-16 gap-8 flex flex-col items-center mx-auto">
-            <h3 className="text-[16px] text-[#717171] uppercase">
-                Guest reviews
-            </h3>
-            <div className="flex flex-row justify-between w-full">
-                {stats.map((stat, index) => (
-                    <CircularRating key={index} {...stat} />
-                ))}
-            </div>
-        </div>
-    );
-}
-
+  return (
+    <div className="w-full max-w-275 mt-16 gap-8 flex flex-col items-center mx-auto">
+      <h3 className="text-[16px] text-[#717171] uppercase">Guest reviews</h3>
+      <div className="flex flex-row justify-between w-full">
+        {stats.map((stat, index) => (
+          <CircularRating key={index} {...stat} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const CircularRating = ({ label, score }) => {
-    const radius = 40;
-    const stroke = 4;
-    const normalizedRadius = radius - stroke * 0.5;
+  const radius = 40;
+  const stroke = 4;
+  const normalizedRadius = radius - stroke * 0.5;
 
-    const circumference = normalizedRadius * 2 * Math.PI;
+  const circumference = normalizedRadius * 2 * Math.PI;
 
-    const percent = score / 10;
-    const strokeDashoffset = circumference * (1 - percent);
+  const percent = score / 10;
+  const strokeDashoffset = circumference * (1 - percent);
 
-    return (
-        <div className="flex flex-col items-center justify-between">
-            <div className="relative w-20 h-30 flex items-center justify-center">
-                <svg
-                    height={radius * 2}
-                    width={radius * 2}
-                >
-                    {/* background circle */}
-                    <circle
-                        stroke="#eee"
-                        fill="transparent"
-                        strokeWidth={stroke}
-                        r={normalizedRadius}
-                        cx={radius}
-                        cy={radius}
-                    />
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <div className="relative w-20 h-30 flex items-center justify-center">
+        <svg height={radius * 2} width={radius * 2}>
+          {/* background circle */}
+          <circle
+            stroke="#eee"
+            fill="transparent"
+            strokeWidth={stroke}
+            r={normalizedRadius}
+            cx={radius}
+            cy={radius}
+          />
 
-                    {/* progress circle */}
-                    <circle
-                        stroke="#581ADB"
-                        fill="transparent"
-                        strokeWidth={stroke}
-                        strokeLinecap="round"
-                        strokeDasharray={circumference}
-                        strokeDashoffset={strokeDashoffset}
-                        r={normalizedRadius}
-                        cx={radius}
-                        cy={radius}
-                        style={{
-                            transition: "stroke-dashoffset 0.5s ease"
-                        }}
-                    />
-                </svg>
+          {/* progress circle */}
+          <circle
+            stroke="#581ADB"
+            fill="transparent"
+            strokeWidth={stroke}
+            strokeLinecap="round"
+            strokeDasharray={circumference}
+            strokeDashoffset={strokeDashoffset}
+            r={normalizedRadius}
+            cx={radius}
+            cy={radius}
+            style={{
+              transition: "stroke-dashoffset 0.5s ease",
+            }}
+          />
+        </svg>
 
-                <span className="absolute text-[32px] text-[#581ADB]">
-                    {score}
-                </span>
-            </div>
+        <span className="absolute text-[32px] text-[#581ADB]">{score}</span>
+      </div>
 
-            <span className="mt-2 text-[16px] uppercase text-[#581ADB]">
-                {label}
-            </span>
-        </div>
-    );
+      <span className="mt-2 text-[16px] uppercase text-[#581ADB]">{label}</span>
+    </div>
+  );
 };
 
 export default ReviewStats;
